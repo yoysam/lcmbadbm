78a62c293ce1f46bf9e770ac8cd7b3e75cff4a97
yosef <yoyosam@optonline.net>
2020-03-19 13:20:28 -0400

    test commit

--- src/main/java/edu/touro/mco152/bm/ui/swingworker.java
+++ src/main/java/edu/touro/mco152/bm/ui/swingworker.java
@@ -1,4 +1,33 @@
 package edu.touro.mco152.bm.ui;
 
-public class swingworker {
+import edu.touro.mco152.bm.DiskMark;
+import edu.touro.mco152.bm.SwitchWorker;
+
+import javax.swing.*;
+
+/**
+ * calls the the swing implemations of the methods
+ */
+public class swingworker extends SwingWorker<Boolean, DiskMark> implements uiworker {
+    @Override
+    public void pusdopublish(DiskMark d) {
+         publish(d);
+    }
+
+    @Override
+    public boolean wascan() {
+        return isCancelled();
+    }
+
+    @Override
+    public void updateprog(int progress) {
+        setProgress(progress);
+
+    }
+
+    @Override
+    protected Boolean doInBackground() throws Exception {
+        SwitchWorker switchWorker=new SwitchWorker(this);
+        return switchWorker.dostuff();
+    }
 }
--- src/test/java/DiskMarkTest.java
+++ src/test/java/DiskMarkTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import edu.touro.mco152.bm.DiskMark;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-public class DiskMarkTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DiskMarkTest {
+    DiskMark test= new DiskMark(DiskMark.MarkType.WRITE);
+    @BeforeAll
+    public static void init(){
+        DiskMark test= new DiskMark(DiskMark.MarkType.WRITE);
+    }
+    @Test
+    public void Crosstest(){
+        test.setBwMbSec(123.33);
+        assertEquals(123.33,test.getBwMbSec());
+    }
 }
